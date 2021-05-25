d62e082f6473d2393f2048b06da83b08b84caa92
Guillermo Lazo <jlazodg@jci.com>
2021-05-25 10:55:20 -0500

    Automation framework set-up

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true
+    },
+    extends: 'standard',
+    plugins: ['promise', 'testcafe'],
+    extends: 'plugin:testcafe/recommended'
+}
